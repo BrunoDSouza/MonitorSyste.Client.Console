4d0ff221659012ba368f6861118cb5fd14f986e6
unknown <brunosouzasantos07@gmail.com>
2018-08-21 00:05:57 -0300

    #Setup enviroment to client console connect to message service

---
+++ MonitorSyste.Client.Console/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using MonitorSystem.Core.Model;
+using MonitorSystem.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace MonitorSystem.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var connection = new HubConnection("http://localhost:52482/signalr");
+                var hub = connection.CreateHubProxy("monitor");
+
+                connection.Start().ContinueWith(task =>
+                {
+                    if (!task.IsFaulted) Console.WriteLine("Connected");
+                    else Console.WriteLine("Aconteceu algum erro durante a conexão com o servidor", task.Exception.GetBaseException());
+                }).Wait();
+
+                hub.On<string>("ReceiveMessage", data => Console.WriteLine(data));
+
+                Thread job = new Thread(() => SendProcessToServer(hub));
+                job.Start();
+
+                Console.Read();
+                connection.Stop();
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void SendProcessToServer(IHubProxy hub)
+        {
+            var computer = new Computer
+            {
+                Ipv4 = SystemHelper.GetIPV4(),
+                NameComputer = SystemHelper.GetNameComputer(),
+                UserName = SystemHelper.GetUserName(),
+                Processes = SystemHelper.GetProcesses(Process.GetProcesses())
+            };
+            var message = new MessageReturnModel
+            {
+                Sucesso = true,
+                Mensagem = new List<string> { "Informações obtidas com sucesso." },
+                Retorno = computer
+            };
+
+            hub.Invoke<string>("SendProcess", message).ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                    Console.WriteLine("Ocorreu algum durante o envio da mensagem ao servidor: {0}", task.Exception.GetBaseException());
+            }).Wait();
+        }
+    }
+}
--- MonitorSyste.Client.Console/Properties/AssemblyInfo.cs
+++ MonitorSyste.Client.Console/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associada a um assembly.
-[assembly: AssemblyTitle("MonitorSystem.Core")]
+[assembly: AssemblyTitle("MonitorSyste.Client.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MonitorSystem.Core")]
+[assembly: AssemblyProduct("MonitorSyste.Client.Console")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("7e1c731e-5ee4-49b3-8546-3b45afc1413b")]
+[assembly: Guid("a0ef1dea-5a2c-4172-bfce-aa6782c51544")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revisão
 //
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
-// usando o '*' como mostrado abaixo:
+// utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
